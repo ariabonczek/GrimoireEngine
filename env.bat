1b20aff2717f0b864a63ec0b7f8d169984717512
abonczek <aria.bonczek@gmail.com>
2018-10-28 13:58:22 -0700

    More cleanup and gitignore updates

--- env.bat
+++ env.bat
@@ -1,11 +1,9 @@
 @echo OFF
 
-SET PROJECT_PATH=C:\Users\Aria\Personal\Code\GrimoireEngine
+SET PROJECT_PATH=%cd%
 SET CODE_PATH=%PROJECT_PATH%\grim-code
 SET DATA_PATH=%PROJECT_PATH%\grim-data
 SET GRIMDAT_PATH=%CODE_PATH%\Tools\GrimDat
 SET PYTHONPATH=%CODE_PATH%\bin\python
-SET QT_PATH=C:\Development\Source\Qt\5.7\msvc2015_64
-SET QML2_IMPORT_PATH=%QT_PATH%\qml\QtQuick\Controls;%QT_PATH%\qml\QtQuick\Controls.2
 
 start cmd
