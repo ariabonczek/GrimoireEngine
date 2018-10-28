5ff36b6e19e367d0549f29c850c50a8417e1fe27
abonczek <aria.bonczek@gmail.com>
2018-10-28 13:50:45 -0700

    Grimoire cleanup - editing gitignore and removing files that shouldn't exist anymore

--- clean.bat
+++
@@ -1,6 +0,0 @@
-@echo OFF
-
-Pushd %GRIMDAT_PATH%
-python grimDat.py -i engine.grimDat -o Generated -j type_header.jinja -p Include.props
-python grimDat.py -i engine.grimDat -o Generated -j type_source.jinja -p Source.props
-popd
